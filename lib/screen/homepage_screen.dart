9b4f1812ea959df39794b9dbf7eca049f176ca4f
yakupbilgen <yakupbilgen92@gmail.com>
2021-05-01 01:31:44 +0300

    BottomNavigatorBar widget created

--- lib/screen/homepage_screen.dart
+++ lib/screen/homepage_screen.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
 
+
 class HomePage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+     
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue,
+        selectedItemColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Colors.blue,
+            icon: Icon(Icons.home),
+            label: 'Anasayfa',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.blue,
+            icon: Icon(Icons.people_alt),
+            label: 'Personel',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.blue,
+            icon: Icon(Icons.source),
+            label: 'Hakkımızda',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.blue,
+            icon: Icon(Icons.contact_mail),
+            label: 'İletişim',
+          ),
+        ],
+      ),
+    );
   }
 }
