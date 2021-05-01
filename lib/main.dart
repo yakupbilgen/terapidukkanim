b818faa8c467677da22def7bf5933b12e8a6971c
yakupbilgen <yakupbilgen92@gmail.com>
2021-05-02 01:29:20 +0300

    url_launcher package install

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import './screen/homepage_screen.dart';
 
 main(List<String> args) {
 runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
   initialRoute: "/",
   routes: {
     "/":(context)=> HomePage(),
