a589944127deb72daecff1c9708af8b3b0cf1515
yakupbilgen <yakupbilgen92@gmail.com>
2021-05-02 04:34:50 +0300

    appointment screen

--- lib/screen/appointment_screen.dart
+++ lib/screen/appointment_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:terapidukkanim/widgets/appointment_card.dart';
 
 class AppointmentScreen extends StatelessWidget {
   String appBarTitle = 'Online Randevu Al';
@@ -9,8 +10,27 @@ class AppointmentScreen extends StatelessWidget {
         centerTitle: true,
         title: Text(appBarTitle),
       ),
-      body: Center(
-        child: Text('data'),
+      body: SingleChildScrollView(
+        child: Center(
+            child: Column(
+          
+          children: [
+            Text('data'),
+            AppointmentCardWidget(
+                imageAsset: 'imageAsset',
+                contentText: 'Bireysel Terapi\n(Yetişkin)',
+                linkUrl: 'linkUrl'),
+            AppointmentCardWidget(
+                imageAsset: 'imageAsset',
+                contentText: 'Bireysel Terapi|n(Ergen)',
+                linkUrl: 'linkUrl'),
+            AppointmentCardWidget(
+                imageAsset: 'imageAsset',
+                contentText: 'Yapılandırılmış Oyun Terapisi\n(Ebeveyn)',
+                linkUrl: 'linkUrl'),
+            Text('data'),
+          ],
+        )),
       ),
     );
   }
